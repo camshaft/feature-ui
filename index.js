e7b17a34b6a71ef821c9cb20edbc3163205ca975
Michael Vanasse <michaelvanasse@gmail.com>
2013-10-24 20:37:04 -0600

    add dom elements and styles

--- index.js
+++ index.js
@@ -18,10 +18,33 @@ module.exports = function(options) {
     .end(function(err, res) {
       if (err) return console.error(err);
       if (!res.ok) return console.error(new Error(res.text));
-
-      // TODO do something with the features list
-      var featureListing = res.body.map(function(name) {
-        return {name: name, enabled: feature(name)};
-      });
+      res.body.forEach(createItem);
     });
+
+
+  var ul = document.createElement('ul');
+  var ui = document.createElement('div');
+  ui.className = 'feature-ui';
+  ui.appendChild(ul);
+
+  function createItem(item) {
+    var li = document.createElement('li');
+    var span = document.createElement('span');
+    span.innerText = item;
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = feature(item);
+    checkbox.onchange = function() {
+      console.log(item, feature(item));
+      if (feature(item)) return feature.disable(item);
+      feature.enable(item);
+    };
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    ul.appendChild(li);
+  }
+
+  document.body.appendChild(ui);
+
 };
+
---
+++ template.js
@@ -0,0 +1,1 @@
+module.exports = '<h1>dfasdfsa</h1>\n';
